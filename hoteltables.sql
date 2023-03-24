16662ec1c5e522d49d46bee68b71d36502d577bf
Jon Klein <jonklein2021@gmail.com>
2023-03-24 13:01:38 -0400

    guest inserts finished

--- hoteltables.sql
+++ hoteltables.sql
@@ -16,13 +16,13 @@ DROP TABLE transactions;
 DROP TABLE guests;
 
 CREATE TABLE ammenities (
-    a_id NUMERIC(3, 0),
+    a_id VARCHAR(3),
     a_name VARCHAR(20),
     PRIMARY KEY(a_id)
 );
 
 CREATE TABLE hotels (
-    h_id NUMERIC(3, 0),
+    h_id VARCHAR(3),
     address VARCHAR(50),
     PRIMARY KEY(h_id)
 );
@@ -41,43 +41,44 @@ CREATE TABLE room_types (
 );
 
 CREATE TABLE rates (
-    rate_usd NUMERIC(4, 0),
+    rate_usd NUMERIC(4, 2),
     rate_points NUMERIC(6, 0),
     PRIMARY KEY(rate_usd) /* Works fine because $1 = 100 points across all hotels **/
 );
 
 CREATE TABLE reservations (
-    res_id NUMERIC(5, 0),
+    res_id VARCHAR(5),
     in_time TIMESTAMP,
     out_time TIMESTAMP,
-    cost_usd NUMERIC(5, 0),
-    cost_points NUMERIC(7, 0),
+    usd NUMERIC(5, 2),
+    points NUMERIC(7, 0),
     cancellation_fee NUMERIC(2, 0),
     PRIMARY KEY(res_id)
 );
 
 CREATE TABLE transactions (
-    t_id NUMERIC(5, 0),
+    t_id VARCHAR(5),
     t_time TIMESTAMP,
-    usd NUMERIC(4, 0),
+    usd NUMERIC(4, 2),
     points NUMERIC(6, 0),
     PRIMARY KEY(t_id)
 );
 
 CREATE TABLE guests (
-    g_id NUMERIC(6, 0),
-    name_first VARCHAR(20),
-    name_last VARCHAR(20),
-    address VARCHAR(50),
-    phone_number NUMERIC(10, 0),
-    cc_number NUMERIC(16, 0),
+    g_id VARCHAR(5),
+    fname VARCHAR(20),
+    lname VARCHAR(20),
+    address VARCHAR(60),
+    email VARCHAR(50),
+    phone_number VARCHAR(14), /* (xxx) xxx-xxxx */
+    cc_number VARCHAR(19), /* xxxx xxxx xxxx xxxx */
     points NUMERIC(6, 0),
     PRIMARY KEY(g_id)
 );
 
 CREATE TABLE has (
-    h_id NUMERIC(3, 0),
-    a_id NUMERIC(3, 0),
+    h_id VARCHAR(3),
+    a_id VARCHAR(3),
     PRIMARY KEY(h_id, a_id),
     FOREIGN KEY(h_id) REFERENCES hotels,
     FOREIGN KEY(a_id) REFERENCES ammenities
@@ -85,7 +86,7 @@ CREATE TABLE has (
 
 
 CREATE TABLE contains (
-    h_id NUMERIC(3, 0),
+    h_id VARCHAR(3),
     r_number NUMERIC(3, 0),
     r_type VARCHAR(20),
     r_capacity NUMERIC(1, 0),
@@ -96,8 +97,8 @@ CREATE TABLE contains (
 );
 
 CREATE TABLE costs (
-    h_id NUMERIC(3, 0),
-    rate_usd NUMERIC(4, 0),
+    h_id VARCHAR(3),
+    rate_usd NUMERIC(4, 2),
     r_type VARCHAR(20),
     r_capacity NUMERIC(1, 0),
     PRIMARY KEY(h_id, rate_usd, r_type, r_capacity),
@@ -107,32 +108,41 @@ CREATE TABLE costs (
 );
 
 CREATE TABLE represents (
-    res_id NUMERIC(5, 0),
-    t_id NUMERIC(5, 0),
+    res_id VARCHAR(5),
+    t_id VARCHAR(5),
     PRIMARY KEY(res_id, t_id),
     FOREIGN KEY(res_id) REFERENCES reservations,
     FOREIGN KEY(t_id) REFERENCES transactions
 );
 
 CREATE TABLE uses (
-    res_id NUMERIC(5, 0),
-    rate_usd NUMERIC(4, 0),
+    res_id VARCHAR(5),
+    rate_usd NUMERIC(4, 2),
     PRIMARY KEY(res_id, rate_usd),
     FOREIGN KEY(res_id) REFERENCES reservations,
     FOREIGN KEY(rate_usd) REFERENCES rates
 );
 
+CREATE TABLE res_type (
+    res_id VARCHAR(5),
+    r_type VARCHAR(20),
+    r_capacity NUMERIC(1, 0),
+    PRIMARY KEY(res_id),
+    FOREIGN KEY(res_id) REFERENCES reservations,
+    FOREIGN KEY(r_type, r_capacity) REFERENCES room_types
+);
+
 CREATE TABLE reserves (
-    g_id NUMERIC(6, 0),
-    res_id NUMERIC(5, 0),
+    g_id VARCHAR(5),
+    res_id VARCHAR(5),
     PRIMARY KEY(g_id, res_id),
     FOREIGN KEY(g_id) REFERENCES guests,
     FOREIGN KEY(res_id) REFERENCES reservations
 );
 
 CREATE TABLE spends (
-    g_id NUMERIC(6, 0),
-    t_id NUMERIC(5, 0),
+    g_id VARCHAR(5),
+    t_id VARCHAR(5),
     PRIMARY KEY(g_id, t_id),
     FOREIGN KEY(g_id) REFERENCES guests,
     FOREIGN KEY(t_id) REFERENCES transactions
