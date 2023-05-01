05323b13d4ceb8ed1f6eeea2ada98d3d37bd3fca
Jon Klein <jonklein2021@gmail.com>
2023-05-01 16:44:57 -0400

    housekeeping draft 1

--- jak325klein/Utilization/Utilization.java
+++ jak325klein/Utilization/Utilization.java
@@ -5,6 +5,9 @@
 
 import java.sql.*;
 import java.time.*; // LocalDate work as SQL timestamp objects
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utilization {
@@ -22,14 +25,18 @@ public class Utilization {
             dbUsername = s.nextLine();
             System.out.print("Password: ");
             dbPassword = s.nextLine();
+            
+            // hide password from lurkers (caleb)
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-            System.out.println("\nAttempting connection...");
+            System.out.println("\nAttempting connection...\n");
 
             try (
                 Connection c = DriverManager.getConnection(url, dbUsername, dbPassword);
             ) {
                 master = false;
                 System.out.println("Connection successful.\n");
+                Thread.sleep(500); // dramatic pause
 
                 while (running) {
                     System.out.println("Welcome to the Hotel California! Please select an operation.");
@@ -77,7 +84,8 @@ public class Utilization {
                             System.out.println("Housekeeping Interface");
                             System.out.println("==================================================");
 
-                            System.out.println("Good evening. Please enter your city.");
+                            System.out.println("Good evening.");
+                            housekeeping(s, c);
 
                             break;
 
@@ -95,15 +103,123 @@ public class Utilization {
             } catch (SQLException e) {
                 if(e.getErrorCode() == 1017) {
                     // login error
-                    System.err.println("Error: Wrong username/password. Please try again.");
+                    System.err.println("SQL Error: Wrong username/password. Please try again.");
                 } else {
-                    // all other errors
-                    System.err.println("Error: Something went wrong.\nStack trace:");
+                    // all other SQL errors
+                    System.err.println("SQL Error: Something went wrong.\nStack trace:");
                     e.printStackTrace();
                     s.close();
                     System.exit(1);
                 };
+            } catch (Exception e) {
+                System.err.println("Error: Something went wrong.\nStack trace:");
+                e.printStackTrace();
+                System.exit(1);
             }
         }
+    }
+
+    public static void housekeeping(Scanner s, Connection c) {
+        ResultSet res;
+        ArrayList<String> hotels = new ArrayList<>(); // store h_ids of results in here
+        String myHotel = "000"; // hotel id to clean within
+
+        ArrayList<String> roomsToClean = (ArrayList<String>) Arrays.asList(s.nextLine().split(" ")); // store input rooms here
+        ArrayList<String> badRooms = new ArrayList<>(); // store clean rooms, occupied rooms, and rooms not in this hotel in here
+
+        int index = 0;
+        boolean tryAgain = true; // to loop for inputs
+
+        try (
+            PreparedStatement findHotels = c.prepareStatement("SELECT * FROM hotels WHERE address LIKE ?");
+            PreparedStatement checkRoom = c.prepareStatement("SELECT * FROM rooms WHERE r_number = ?");
+            PreparedStatement cleanRooms = c.prepareStatement("UPDATE rooms SET is_clean = 1 WHERE r_number = ?");
+        ) {
+            while (tryAgain) {
+                System.out.print("Please enter your hotel's city: ");
+                String city = s.nextLine();
+                findHotels.setString(1, "%, " + city + ", %");
+            
+                res = findHotels.executeQuery();
+                if (!res.next() || city.equals("%")) {
+                    System.err.printf("No hotels in city \"%s\" found. Please try again.\n", city);
+                } else {
+                    tryAgain = false;
+                    System.out.printf("%-7s%-10s%-10s\n", "Index", "Hotel ID", "Hotel Address");
+                    do {
+                        hotels.add(res.getString("h_id"));
+                        System.out.printf("%-7s%-10s%-10s\n", ++index, hotels.get(index-1), res.getString("address"));
+                    } while (res.next());
+                }
+            }
+
+            // select the correct hotel using index if there are multiple with the same city name
+            tryAgain = true;
+            while (tryAgain) {
+                if (index > 1) {
+                    System.out.print("\nPlease enter the index of the hotel you would like to select: ");
+                    try {
+                        int i = s.nextInt();
+                        if (i > 0 && i <= index) { // ensure that index is in range
+                            myHotel = hotels.get(i); // clean rooms in this hotel only
+                            tryAgain = false;
+                        } else {                    
+                            System.err.println("Input error: Please enter a valid number");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.err.println("Input error: Please enter a valid number");
+                    }
+                }
+            }
+
+            System.out.println("Please enter the room numbers that you have cleaned separated by a space (ex: \"40129 40130 40131\")\nNote: Invalid rooms will not be altered");
+
+            for (int i = 0; i < roomsToClean.size(); i++) {
+                String room = roomsToClean.get(i);
+                if (!room.substring(0, 2).equals(myHotel.substring(1))) {
+                    badRooms.add(room); // is the room inside the proper hotel?
+                    roomsToClean.remove(i);
+                } else { // is it occupied or clean? is it a valid room number at all?
+                    checkRoom.setString(1, room);
+                    res = checkRoom.executeQuery();
+                    if (!res.next()) {
+                        badRooms.add(room); // not in rooms table
+                        roomsToClean.remove(i);
+                    } else {
+                        do {
+                            if (res.getInt("is_vacant") == 0 || res.getInt("is_clean") == 1) {
+                                badRooms.add(room); // room is occupied and/or already cleaned
+                                roomsToClean.remove(i);
+                            }
+                        } while (res.next());
+                    }
+                }
+            }
+
+            System.out.println("Cleaning...");
+
+            // set is_clean column in roomsToClean to 1 for all rooms
+            for (String room : roomsToClean) {
+                cleanRooms.setString(1, room);
+                cleanRooms.executeQuery();
+            }
+
+            System.out.println("Rooms cleaned successfully!");
+
+            if (badRooms.size() > 0) {
+                System.out.println("WARNING: The following rooms are either invalid, clean, or occupied and will not be altered:");
+                for (String room : badRooms) {
+                    System.out.print(room + " ");
+                }
+                System.out.println("");
+            }
+            
+        } catch (SQLException e) {
+            System.err.println("Error: Something went wrong.\nStack trace:");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        
     }
 }
